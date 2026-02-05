2fd9b88717dfcc0e82c220f1e1b6b9a34a3162d7
Trương Minh Việt <minhviet14022007@gmail.com>
2026-02-05 08:48:43 +0700

    nopdemo

--- bai1.c
+++ bai1.c
@@ -10,4 +10,31 @@
 
 
 // VIẾT CODE Ở ĐÂY
-
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    int mang[n];
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i);
+        scanf("%d", &mang[i]);
+    }
+    float tong = 0;
+    float tb;
+    int count = 0;
+    for (i = 0; i < n; i++) {
+        if (mang[i] % 3 == 0) {
+            tong = tong + mang[i];
+            count++;
+        }
+    }
+    if (count > 0) {
+        tb = tong / count;
+        printf("Trung binh cong cac so chia het cho 3 la: %.2f\n", tb);
+    } else {
+        printf("Trong mang khong co so nao chia het cho 3.\n");
+    }
+    return 0;
+}
